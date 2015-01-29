7ea268f71fdeff8c5ca760f330698ea5f1dda702
Chris Davies <chris.davies@theironyard.com>
2015-01-28 20:29:24 -0500

    Moving to crockford's object approach

--- rlite.js
+++ rlite.js
@@ -1,71 +1,77 @@
 function Rlite() {
-    this.rules = {};
-}
+  var routes = {};
 
-Rlite.prototype = {
-    add: function (route, handler) {
-        var pieces = route.split('/'),
-            rules = this.rules;
+  var self = {
+    add: function(route, handler) {
+      var pieces = route.split('/'),
+          rules = routes;
 
-        for (var i = 0; i < pieces.length; ++i) {
-            var piece = pieces[i],
-                name = piece.length && piece.charAt(0) == ':' ? ':' : piece;
+      for (var i = 0; i < pieces.length; ++i) {
+        var piece = pieces[i],
+            name = piece.length && piece.charAt(0) == ':' ? ':' : piece;
 
-            if (!rules[name]) {
-                rules = (rules[name] = {});
+        if (!rules[name]) {
+          rules = (rules[name] = {});
 
-                if (name == ':') {
-                    rules['@name'] = piece.substr(1, piece.length - 1);
-                }
-            } else {
-                rules = rules[name];
-            }
+          if (name == ':') {
+            rules['@name'] = piece.slice(1);
+          }
+        } else {
+          rules = rules[name];
         }
+      }
 
-        rules['@'] = handler;
+      rules['@'] = handler;
     },
 
-    run: function (url) {
-        if (url && url.length) {
-            url = url.replace('/?', '?');
-            url.charAt(0) == '/' && (url = url.substr(1, url.length));
-            url.length && url.charAt(url.length - 1) == '/' && (url = url.substr(0, url.length - 1));
+    run: function(url) {
+      var rules = routes;
+      
+      if (url && url.length) {
+        url = url.replace('/?', '?');
+        url.charAt(0) == '/' && (url = url.substr(1, url.length));
+        url.length && url.charAt(url.length - 1) == '/' && (url = url.substr(0, url.length - 1));
+      }
+
+      var querySplit = url.split('?', 2),
+          pieces = querySplit[0].split('/', 50),
+          params = {};
+
+      (function parseUrl() {
+        for (var i = 0; i < pieces.length && rules; ++i) {
+          var piece = pieces[i],
+            lower = piece.toLowerCase(),
+            rule = rules[lower];
+
+          if (!rule && (rule = rules[':'])) {
+            params[rule['@name']] = piece;
+          }
+
+          rules = rule;
         }
+      })();
 
-        var rules = this.rules,
-            querySplit = url.split('?', 2),
-            pieces = querySplit[0].split('/', 50),
-            params = {};
+      (function parseQuery(q) {
+        var query = q.split('&', 50);
 
-        (function parseUrl() {
-            for (var i = 0; i < pieces.length && rules; ++i) {
-                var piece = pieces[i],
-                    lower = piece.toLowerCase(),
-                    rule = rules[lower];
+        for (var i = 0; i < query.length; ++i) {
+          var nameValue = query[i].split('=', 2);
 
-                if (!rule && (rule = rules[':'])) {
-                    params[rule['@name']] = piece;
-                }
-
-                rules = rule;
-            }
-        })();
-
-        (function parseQuery(q) {
-            var query = q.split('&', 50);
-
-            for (var i = 0; i < query.length; ++i) {
-                var nameValue = query[i].split('=', 2);
-
-                nameValue.length == 2 && (params[nameValue[0]] = nameValue[1]);
-            }
-        })(querySplit.length == 2 ? querySplit[1] : '');
-
-        if (rules && rules['@']) {
-            rules['@']({ url: url, params: params });
-            return true;
+          nameValue.length == 2 && (params[nameValue[0]] = nameValue[1]);
         }
+      })(querySplit.length == 2 ? querySplit[1] : '');
 
-        return false;
+      if (rules && rules['@']) {
+        rules['@']({
+          url: url,
+          params: params
+        });
+        return true;
+      }
+
+      return false;
     }
-};
+  };
+
+  return self;
+}

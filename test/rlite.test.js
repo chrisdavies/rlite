a4028c23ad44ff3a1665c700657a7bb9894211b7
Chris Davies <chris.davies@theironyard.com>
2015-01-29 06:39:42 -0500

    Adding Rlite.handlers

--- test/rlite.test.js
+++ test/rlite.test.js
@@ -1,132 +1,155 @@
-﻿test('Route parameter check', function () {
-    var r = new Rlite();
+﻿
+test('Route parameter check', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name', function (r) {
-        ok(r.params.name == 'chris');
-    });
+  r.add('hey/:name', function(r) {
+    ok(r.params.name == 'chris');
+  });
 
-    r.run('hey/chris');
+  r.run('hey/chris');
 });
 
-test('Other routes don\'t get confused', function () {
-    var r = new Rlite();
+test('Other routes don\'t get confused', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name/new', function (r) {
-        throw 'New called';
-    });
+  r.add('hey/:name/new', function(r) {
+    throw 'New called';
+  });
 
-    r.add('hey/:name', function (r) {
-        ok(r.params.name == 'chris');
-    });
+  r.add('hey/:name', function(r) {
+    ok(r.params.name == 'chris');
+  });
 
-    r.add('hey/:name/edit', function (r) {
-        throw 'Edit called';
-    });
+  r.add('hey/:name/edit', function(r) {
+    throw 'Edit called';
+  });
 
-    r.run('hey/chris');
+  r.run('hey/chris');
 });
 
-test('Most explicit wins', function () {
-    var r = new Rlite();
+test('Most explicit wins', function() {
+  var r = new Rlite();
 
-    r.add('hey/joe', function (r) {
-        ok(r.url == 'hey/joe');
-    });
+  r.add('hey/joe', function(r) {
+    ok(r.url == 'hey/joe');
+  });
 
-    r.add('hey/:name', function (r) {
-        throw 'New called';
-    });
+  r.add('hey/:name', function(r) {
+    throw 'New called';
+  });
 
-    r.add('hey/jane', function (r) {
-        ok(r.url == 'hey/jane');
-    });
+  r.add('hey/jane', function(r) {
+    ok(r.url == 'hey/jane');
+  });
 
-    r.run('hey/joe');
-    r.run('hey/jane');
+  r.run('hey/joe');
+  r.run('hey/jane');
 });
 
-test('Complex routes work', function () {
-    var r = new Rlite();
+test('Complex routes work', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name/new', function (r) {
-        throw 'New called';
-    });
+  r.add('hey/:name/new', function(r) {
+    throw 'New called';
+  });
 
-    r.add('hey/:name', function (r) {
-        throw 'Name called';
-    });
+  r.add('hey/:name', function(r) {
+    throw 'Name called';
+  });
 
-    r.add('hey/:name/last/:last', function (r) {
-        ok(r.params.name == 'chris' && r.params.last == 'davies');
-    });
+  r.add('hey/:name/last/:last', function(r) {
+    ok(r.params.name == 'chris' && r.params.last == 'davies');
+  });
 
-    r.run('hey/chris/last/davies');
+  r.run('hey/chris/last/davies');
 });
 
-test('Query strings override other params', function () {
-    var r = new Rlite();
+test('Query strings override other params', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name/new', function (r) {
-        throw 'New called';
-    });
+  r.add('hey/:name/new', function(r) {
+    throw 'New called';
+  });
 
-    r.add('hey/:name', function (r) {
-        throw 'Name called';
-    });
+  r.add('hey/:name', function(r) {
+    throw 'Name called';
+  });
 
-    r.add('hey/:name/last/:last', function (r) {
-        ok(r.params.name == 'ham' && r.params.last == 'mayo');
-    });
+  r.add('hey/:name/last/:last', function(r) {
+    ok(r.params.name == 'ham' && r.params.last == 'mayo');
+  });
 
-    ok(r.run('hey/chris/last/davies?last=mayo&name=ham'));
+  ok(r.run('hey/chris/last/davies?last=mayo&name=ham'));
 });
 
-test('Not found', function () {
-    var r = new Rlite();
+test('Not found', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name', function (r) {
-        throw 'Name called';
-    });
+  r.add('hey/:name', function(r) {
+    throw 'Name called';
+  });
 
-    ok(!r.run('hoi/there'));
+  ok(!r.run('hoi/there'));
 });
 
-test('Leading slashes don\'t matter', function () {
-    var r = new Rlite();
+test('Leading slashes don\'t matter', function() {
+  var r = new Rlite();
 
-    r.add('hey/:name', function (r) {
-        ok(r.params.name == 'chris');
-    });
+  r.add('hey/:name', function(r) {
+    ok(r.params.name == 'chris');
+  });
 
-    r.run('/hey/chris');
+  r.run('/hey/chris');
 });
 
-test('Default url', function () {
-    var r = new Rlite();
+test('Default url', function() {
+  var r = new Rlite();
 
-    r.add('', function (r) {
-        ok(true);
-    });
+  r.add('', function(r) {
+    ok(true);
+  });
 
-    r.run('');
+  r.run('');
 });
 
-test('Trailing slash', function () {
-    var r = new Rlite();
+test('Trailing slash', function() {
+  var r = new Rlite();
 
-    r.add('hoi', function (r) {
-        ok(true);
-    });
+  r.add('hoi', function(r) {
+    ok(true);
+  });
 
-    r.run('hoi/');
+  r.run('hoi/');
 });
 
-test('Trailing slash with query', function () {
-    var r = new Rlite();
+test('Trailing slash with query', function() {
+  var r = new Rlite();
 
-    r.add('hoi', function (r) {
-        ok(true);
-    });
+  r.add('hoi', function(r) {
+    ok(true);
+  });
 
-    r.run('hoi/?there');
+  r.run('hoi/?there');
+});
+
+
+test('Rlite.handlers calls each handler', function() {
+  var r = new Rlite(),
+      h1Ran = false,
+      h2Ran = false;
+
+  function h1(r) {
+    h1Ran = (r.params.name == 'chris');
+  }
+
+  function h2(r) {
+    h2Ran = (r.params.name == 'chris');
+  }
+
+  r.add('hey/:name', Rlite.handlers(h1, h2));
+
+  r.run('/hey/chris');
+
+  ok(h1Ran);
+  ok(h2Ran);
 });
